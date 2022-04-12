eae87966d4e21113f1c3dca9b6dd5862c29678c1
chibuzodanel <chibuzodaniel71@gmail.com>
2022-04-12 05:06:55 +0100

    final commit

---
+++ Js/script.js
@@ -0,0 +1,13 @@
+// typing animation
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Google developer",
+    "Android developer",
+    "Angular Developer",
+    "Web Designer",
+  ],
+  typeSpeed: 100,
+  backspeed: 60,
+  loop: true,
+});
